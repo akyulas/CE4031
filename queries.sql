69238ce89febc8a5f2e118f25269752cf3cee89e
akyulas <bolshack_fire@hotmail.com>
2019-10-01 03:51:58 +0800

    Updated queries 4, 5 and 6

--- queries.sql
+++ queries.sql
@@ -27,15 +27,28 @@ FROM
 GROUP BY YearDivision
 
 --query 4
---todo
-
+select T3.author, T3.cnt from (
+select author, count(*) as cnt from (
+select A1.author_name as author, A2.author_name as co_author from authored A1 join (
+select * from publications where (category = 'inproceedings'
+and crossRef in (select pubkey from publications where category = 'proceedings'
+and lower(title) like '%data%')) 
+or (category = 'article' and lower(journal) like '%data%')
+)T1 on A1.publication_key = T1.pubkey join authored A2 on A1.publication_key = A2.publication_key
+and A1.author_name <> A2.author_name) T2 group by author) T3 order by cnt desc limit 1;
 
 --query 5
 select *
 from (
-	select author_name,count(*) as pub_count
-	from authored inner join author on authored.author_name = author.name 
-	inner join publications on publications.pubkey = authored.publication_key
+	select author_name, count(*) as pub_count
+	from authored join (
+	select * from publications where 
+	(category = 'inproceedings'
+		and crossRef in (select pubkey from publications where category = 'proceedings'
+		and lower(title) like '%data%')) 
+	or (category = 'article' and lower(journal) like '%data%') 
+	) T1
+	on T1.pubkey = authored.publication_key
 	group by author_name
 )T
 order by pub_count DESC
@@ -43,6 +56,13 @@ LIMIT 10;
 
 --query 6
 
+select title from publications
+where category = 'proceedings'
+and lower(title) like '%data%'
+and pubkey in (
+select crossref from(
+select crossref, count(*) as cnt from publications where category = 'inproceedings'
+group by crossref) T1 where T1.cnt > 100);
 
 --query 7a)
 select author.name, authored.author_name
